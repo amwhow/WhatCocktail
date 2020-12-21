11988ad08731f97ae88f96d206bdefe74edf0741
Mark <amwhow@gmail.com>
2020-12-21 14:12:55 +1100

    create a random generator button inside the modal

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-randomAPI = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-button = document.querySelector("#random").addEventListener("click", () => {
-  let data = fetch(randomAPI)
-  .then(data => data.json())
-  .then(response => {
-    wineName = document.getElementById("cocktailName");
-    wineName.textContent = response["drinks"][0]["strDrink"]
-  })
-})
+// return a random cocktail's name
+
+function randomCocktail() {
+  randomAPI = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  button = document.querySelectorAll("#randomName")
+  for (i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => {
+      wineName.innerHTML = "<div class='spinner-grow text-primary' role='status'><span class='sr-only'></span></div>"
+      let data = fetch(randomAPI)
+      .then(data => data.json())
+      .then(response => {
+        wineName.textContent = response["drinks"][0]["strDrink"]
+      })
+    })
+  }
+  wineName = document.getElementById("cocktailName");
+}
+randomCocktail()
